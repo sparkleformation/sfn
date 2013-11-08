b82f484add1ded4493ce32fc6c382464317f32be
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-08 13:45:56 -0800

    Add knife subcommand options to be passed into exporter instance

--- lib/chef/knife/cloudformation_export.rb
+++ lib/chef/knife/cloudformation_export.rb
@@ -17,6 +17,32 @@ class Chef
         :proc => lambda{|v| Chef::Config[:knife][:cloudformation][:export_path] = v }
       )
 
+      option(:ignore_parameters,
+        :short => '-P NAME',
+        :long => '--exclude-parameter NAME',
+        :description => 'Exclude parameter from export (can be used multiple times)',
+        :proc => lambda{|v|
+          Chef::Config[:knife][:cloudformation][:ignore_parameters] ||= []
+          Chef::Config[:knife][:cloudformation][:ignore_parameters].push(v).uniq!
+        }
+      )
+
+      option(:chef_environment_parameter,
+        :long => '--chef-environment-parameter NAME',
+        :description => 'Parameter used within stack to specify Chef environment',
+        :proc => lambda{|v|
+          Chef::Config[:knife][:cloudformation][:chef_environment_parameter] = v
+        }
+      )
+
+      option(:chef_popsicle,
+        :long => '--[no-]freeze-run-list',
+        :boolean => true,
+        :default => true,
+        :description => 'Freezes first run files',
+        :proc => lambda{|v| Chef::Config[:knife][:cloudformation][:chef_popsicle] = v }
+      )
+
       def run
         if(Chef::Config[:knife][:cloudformation][:export_path])
           file_path = File.join(
@@ -30,7 +56,13 @@ class Chef
           ui.info "  - Printing to console"
         end
         ui.confirm 'Perform export'
-        exporter = KnifeCloudformation::Export.new(name_args.first, :aws_commons => aws)
+        ex_opts = {}
+        [:chef_popsicle, :chef_environment_parameter, :ignore_parameters].each do |key|
+          unless(Chef::Config[:knife][:cloudformation][key].nil?)
+            ex_opts[key] = Chef::Config[:knife][:cloudformation][key]
+          end
+        end
+        exporter = KnifeCloudformation::Export.new(name_args.first, ex_opts.merge(:aws_commons => aws))
         result = exporter.export
         if(file_path)
           File.open(file_path, 'w') do |file|
