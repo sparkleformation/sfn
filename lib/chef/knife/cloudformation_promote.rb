bb29542cb46a3d408033bd5e151b0175cba2e5e1
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-29 10:39:39 -0700

    Include basic skeleton

---
+++ lib/chef/knife/cloudformation_promote.rb
@@ -0,0 +1,40 @@
+require 'knife-cloudformation/cloudformation_base'
+
+class Chef
+  class Knife
+    class CloudformationPromote < Knife
+
+      include KnifeCloudformation::KnifeBase
+
+      banner 'knife cloudformation promote NEW_STACK_NAME DESTINATION'
+
+      option(:accounts,
+        :long => '--accounts-file PATH',
+        :short => '-A PATH',
+        :description => 'JSON account file',
+        :proc => lambda{|v|
+          Chef::Config[:knife][:cloudformation][:promote_accounts] = JSON.load(File.read(v))
+        }
+      )
+
+      option(:storage_bucket,
+        :long => '--exports-bucket NAME',
+        :description => 'Bucket name containing the exports',
+        :proc => lambda{|v| Chef::Config[:knife][:cloudformation][:promote_exports_bucket] = v }
+      )
+
+      option(:storage_prefix,
+        :long => '--exports-prefix PREFIX',
+        :description => 'Prefix of stack key',
+        :proc => lambda{|v| Chef::Config[:knife][:cloudformation][:promote_exports_prefix] = v }
+      )
+
+
+      def run
+        stack_name, destination = name_args
+
+      end
+
+    end
+  end
+end
