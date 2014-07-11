9ec7fe548feb498b2e415d9f4542e1411d44ba1f
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-11 11:52:25 -0700

    Include loaders

--- lib/knife-cloudformation.rb
+++ lib/knife-cloudformation.rb
@@ -1,1 +1,12 @@
 require 'knife-cloudformation/version'
+
+module KnifeCloudformation
+
+  autoload :Provider, 'knife-cloudformation/provider'
+  autoload :Cache, 'knife-cloudformation/cache'
+  autoload :Export, 'knife-cloudformation/export'
+  autoload :KnifeBase, 'knife-cloudformation/cloudformation_base'
+  autoload :Utils, 'knife-cloudformation/utils'
+  autoload :MonkeyPatch, 'knife-cloudformation/monkey_patch'
+
+end
