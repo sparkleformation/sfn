8cdb8b6c6e387fcc6c78eccdfaf19b225284158b
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-11 16:23:45 -0700

    Include knife command loaders

--- lib/knife-cloudformation.rb
+++ lib/knife-cloudformation.rb
@@ -10,3 +10,19 @@ module KnifeCloudformation
   autoload :MonkeyPatch, 'knife-cloudformation/monkey_patch'
 
 end
+
+class Chef
+  class Knife
+    autoload :CloudformationCreate, 'chef/knife/cloudformation_create'
+    autoload :CloudformationDescribe, 'chef/knife/cloudformation_describe'
+    autoload :CloudformationDestroy, 'chef/knife/cloudformation_destroy'
+    autoload :CloudformationEvents, 'chef/knife/cloudformation_events'
+    autoload :CloudformationExport, 'chef/knife/cloudformation_export'
+    autoload :CloudformationImport, 'chef/knife/cloudformation_import'
+    autoload :CloudformationInspect, 'chef/knife/cloudformation_inspect'
+    autoload :CloudformationList, 'chef/knife/cloudformation_list'
+    autoload :CloudformationPromote, 'chef/knife/cloudformation_promote'
+    autoload :CloudformationUpdate, 'chef/knife/cloudformation_update'
+    autoload :CloudformationValidate, 'chef/knife/cloudformation_validate'
+  end
+end
