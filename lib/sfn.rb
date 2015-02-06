5b78fda97424357a0b6991733a23c8e3b695a674
Chris Roberts <code@chrisroberts.org>
2015-02-05 20:08:01 -0800

    Add config autoloader

--- lib/sfn.rb
+++ lib/sfn.rb
@@ -5,6 +5,7 @@ module Sfn
 
   autoload :Provider, 'sfn/provider'
   autoload :Cache, 'sfn/cache'
+  autoload :Config, 'sfn/config'
   autoload :Export, 'sfn/export'
   autoload :Utils, 'sfn/utils'
   autoload :MonkeyPatch, 'sfn/monkey_patch'
