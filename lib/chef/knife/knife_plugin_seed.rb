6e6f65e92f1b8abc5e4990301458c0f0454271f3
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-08 11:10:02 -0800

    Automatically hook commands into knife subcommands

--- lib/chef/knife/knife_plugin_seed.rb
+++ lib/chef/knife/knife_plugin_seed.rb
@@ -1,60 +1,67 @@
-require 'sfn'
+unless(defined?(Chef::Knife::CloudformationCreate))
+  require 'sfn'
 
-class Chef
-  class Knife
+  BOOLEAN_VALUES = [TrueClass, FalseClass]
 
-    Sfn::Config.constants.map do |konst|
-      const = Sfn::Config.const_get(konst)
-      if(const.is_a?(Class) && const.ancestors.include?(Bogo::Config))
-        const
+  Sfn::Config.constants.map do |konst|
+    const = Sfn::Config.const_get(konst)
+    if(const.is_a?(Class) && const.ancestors.include?(Bogo::Config))
+      const
+    end
+  end.compact.sort_by(&:to_s).each do |klass|
+
+    klass_name = klass.name.split('::').last
+    command_class = "Cloudformation#{klass_name}"
+    shorts = []
+
+    knife_klass = Class.new(Chef::Knife)
+    knife_klass.class_eval do
+      # Stub in names so knife will detect
+      def self.name
+        @name
       end
-    end.compact.sort_by(&:to_s).each do |klass|
 
-      klass_name = klass.name.split('::').last
-      command_class = "Cloudformation#{klass_name}"
+      def name
+        self.class.name
+      end
 
-      shorts = []
-
-      command klass_name do
-        if(klass.const_defined?(:DESCRIPTION))
-          description klass.const_get(:DESCRIPTION)
-        end
-        Sfn::Config.attributes.merge(klass.attributes).sort_by(&:first).each do |name, info|
-          next unless info[:description]
-          short = name.chars.zip(name.chars.map(&:upcase)).flatten.detect do |c|
-            !shorts.include?(c)
-          end
-          shorts << short
-          bool = [info[:type]].compact.flatten.all?{|x| BOOLEAN_VALUES.include?(x) }
-          on_name = bool ? name.to_s : "#{name}="
-          on short, on_name.tr('_', '-'), info[:description], :default => info[:default]
-        end
-
-        run do |opts, args|
-          klass.new({klass_name => opts}, args)
-        end
+      def run
+        puts "WE SHOULD DO STUFF!"
       end
 
     end
+    knife_klass.instance_variable_set(:@name, "Chef::Knife::#{command_class}")
+    knife_klass.banner "knife cloudformation #{Bogo::Utility.snake(klass_name)}"
 
+    Sfn::Config.attributes.merge(klass.attributes).sort_by(&:first).each do |name, info|
+      next unless info[:description]
+      short = name.chars.zip(name.chars.map(&:upcase)).flatten.detect do |c|
+        !shorts.include?(c)
+      end
+      shorts << short
+      bool = [info[:type]].compact.flatten.all?{|x| BOOLEAN_VALUES.include?(x) }
+      if(bool)
+        short = "-#{short}"
+        long = "--[no-]#{name.tr('_', '-')}"
+      else
+        short = "-#{short} VALUE"
+        long = "--#{name.tr('_', '-')} VALUE"
+      end
+      knife_klass.option(
+        name.to_sym, {
+          :short => short,
+          :long => long,
+          :boolean => bool,
+          :default => info[:default],
+          :description => info[:description]
+        }
+      )
+    end
 
+    # Set the class as a proper constant
+    Chef::Knife.const_set(command_class, knife_klass)
+    # Force knife to pick up as a subcommand
+    Chef::Knife.inherited(knife_klass)
 
-require 'sparkle_formation'
-require 'pathname'
-require 'knife-cloudformation'
-
-class Chef
-  class Knife
-    # Cloudformation create command
-    class CloudformationCreate < Knife
-
-      include KnifeCloudformation::Knife::Base
-      include KnifeCloudformation::Knife::Template
-      include KnifeCloudformation::Knife::Stack
-
-      banner 'knife cloudformation create NAME'
-
-      option(:timeout,
-        :short => '-t MIN',
-        :long => '--timeout MIN',
-        :description => 'Set timeout for stack creation',
+  end
+end
