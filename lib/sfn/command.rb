8a7e3788a538c0f9f555d3fe37820964907efb41
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-08 11:50:34 -0800

    Fix toplevel namespace type. Remove cloudformation prefixing.

--- lib/sfn/command.rb
+++ lib/sfn/command.rb
@@ -1,19 +1,20 @@
 require 'sfn'
+require 'bogo-cli'
 
-class Sfn
+module Sfn
   class Command < Bogo::Cli::Command
 
-    autoload :CloudformationCreate, 'sfn/command/cloudformation_create'
-    autoload :CloudformationDescribe, 'sfn/command/cloudformation_describe'
-    autoload :CloudformationDestroy, 'sfn/command/cloudformation_destroy'
-    autoload :CloudformationEvents, 'sfn/command/cloudformation_events'
-    autoload :CloudformationExport, 'sfn/command/cloudformation_export'
-    autoload :CloudformationImport, 'sfn/command/cloudformation_import'
-    autoload :CloudformationInspect, 'sfn/command/cloudformation_inspect'
-    autoload :CloudformationList, 'sfn/command/cloudformation_list'
-    autoload :CloudformationPromote, 'sfn/command/cloudformation_promote'
-    autoload :CloudformationUpdate, 'sfn/command/cloudformation_update'
-    autoload :CloudformationValidate, 'sfn/command/cloudformation_validate'
+    autoload :Create, 'sfn/command/create'
+    autoload :Describe, 'sfn/command/describe'
+    autoload :Destroy, 'sfn/command/destroy'
+    autoload :Events, 'sfn/command/events'
+    autoload :Export, 'sfn/command/export'
+    autoload :Import, 'sfn/command/import'
+    autoload :Inspect, 'sfn/command/inspect'
+    autoload :List, 'sfn/command/list'
+    autoload :Promote, 'sfn/command/promote'
+    autoload :Update, 'sfn/command/update'
+    autoload :Validate, 'sfn/command/validate'
 
   end
 end
