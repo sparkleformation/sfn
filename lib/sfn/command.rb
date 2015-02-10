014804925df61748c79ef6428fe73eac93a45784
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-10 07:08:20 -0800

    Include helper to traverse FS for .sfn file discovery

--- lib/sfn/command.rb
+++ lib/sfn/command.rb
@@ -16,5 +16,30 @@ module Sfn
     autoload :Update, 'sfn/command/update'
     autoload :Validate, 'sfn/command/validate'
 
+    # Override to provide config file searching
+    def initialize(opts, args)
+      unless(opts[:config])
+        opts = opts.to_hash.to_smash(:snake)
+        discover_config(opts)
+      end
+      super
+    end
+
+    protected
+
+    # Start with current working directory and traverse to root
+    # looking for a `.sfn` configuration file
+    #
+    # @param opts [Smash]
+    # @return [Smash]
+    def discover_config(opts)
+      cwd = Dir.pwd.split(File::SEPARATOR)
+      until(cwd.empty? || File.exists?(cwd.push('.sfn').join(File::SEPARATOR)))
+        cwd.pop(2)
+      end
+      opts[:config] = cwd.join(File::SEPARATOR) unless cwd.empty?
+      opts
+    end
+
   end
 end
