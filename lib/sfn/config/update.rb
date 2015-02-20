a21284ffa146bbf682d6b0015c700d8500f66a69
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-20 05:32:09 -0800

    Remove duplicated configuration attribute (is already inherited)

--- lib/sfn/config/update.rb
+++ lib/sfn/config/update.rb
@@ -10,11 +10,6 @@ module Sfn
         :description => 'Print the resulting stack template'
       )
       attribute(
-        :apply_nesting, [TrueClass, FalseClass],
-        :default => true,
-        :description => 'Apply nested stacks. When disabled, stacks built in serial order.'
-      )
-      attribute(
         :apply_stack, String,
         :multiple => true,
         :description => 'Apply outputs from stack to input parameters'
