a0ffaaa73c9242caf81bb20fffa4594f2b835ac8
Chris Roberts <code@chrisroberts.org>
2015-09-03 15:32:52 -0700

    Make callback extraction and execution a bit simpler

--- lib/sfn/command_module/callbacks.rb
+++ lib/sfn/command_module/callbacks.rb
@@ -15,9 +15,9 @@ module Sfn
       # @return [Object] result of yield block
       def api_action!(*args)
         type = self.class.name.split('::').last
-        run_callbacks_for(["before_#{type}", :before, :default], *args)
+        run_callbacks_for(["before_#{type}", :before], *args)
         result = yield if block_given?
-        run_callbacks_for(["after_#{type}", :after, :default], *args)
+        run_callbacks_for(["after_#{type}", :after], *args)
         result
       end
 
@@ -27,9 +27,9 @@ module Sfn
       # @return [NilClass]
       def run_callbacks_for(type, *args)
         clbks = [type].flatten.compact.map do |c_type|
-          res_type = c_type.to_sym == :default ? [type].flatten.compact : [c_type]
-          callbacks_for(c_type, res_type)
-        end.flatten.compact.uniq.each do |callback_name, callback|
+          callbacks_for(c_type)
+        end.flatten(1).compact.uniq.each do |item|
+          callback_name, callback = item
           ui.info "Callback #{ui.color(type.to_s, :bold)} #{callback_name}: #{ui.color('starting', :yellow)}"
           if(args.empty?)
             callback.call
@@ -46,9 +46,8 @@ module Sfn
       # @param type [Symbol, String] name of callback type
       # @param responder [Array<String, Symbol>] matching response methods
       # @return [Array<Method>]
-      def callbacks_for(type, responder=[])
-        responder = [type].compact.flatten if resopnder.empty?
-        [config.fetch(:callbacks, type, [])].flatten.compact.map do |c_name|
+      def callbacks_for(type)
+        ([config.fetch(:callbacks, type, [])].flatten.compact + [config.fetch(:callbacks, :default, [])].flatten.compact).map do |c_name|
           instance = memoize(c_name) do
             begin
               klass = Sfn::Callback.const_get(Bogo::Utility.camel(c_name.to_s))
@@ -57,12 +56,10 @@ module Sfn
               raise "Unknown #{type} callback requested: #{c_name} (not found)"
             end
           end
-          responder.map do |m_name|
-            if(instance.respond_to?(m_name))
-              [c_name, instance.method(m_name)]
-            end
-          end.compact
-        end.compact.flatten(1)
+          if(instance.respond_to?(type))
+            [c_name, instance.method(type)]
+          end
+        end.compact
       end
 
     end
