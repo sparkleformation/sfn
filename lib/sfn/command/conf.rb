f33668ac394889fd5bb5ae1f44e1234b95340b39
Chris Roberts <code@chrisroberts.org>
2016-01-21 13:40:04 -0800

    Add credential entries for all currently supported providers

--- lib/sfn/command/conf.rb
+++ lib/sfn/command/conf.rb
@@ -59,8 +59,12 @@ module Sfn
 # options, please see:
 # http://www.sparkleformation.io/docs/sfn/configuration.html
 Configuration.new do
+  #   Set style of stack nesting
   apply_nesting 'deep'
+  #   Enable processing of SparkleFormation templates
   processing true
+  #   Provider specific options used when creating
+  #   new stacks. Options defined here are AWS specific.
   options do
     on_failure 'nothing'
     notification_topics []
@@ -69,14 +73,50 @@ Configuration.new do
       creator ENV['USER']
     end
   end
+  #   Name of bucket in object store to hold nested
+  #   stack templates
   # nesting_bucket 'BUCKET_NAME'
+  #   Prefix used on generated template path prior to storage
+  #   in the object store
+  # nesting_prefix 'nested-templates'
+  #   Remote provider credentials
   credentials do
+    #  Remote provider name (:aws, :azure, :open_stack, :rackspace)
     provider :aws
+    #  AWS credentials information
     aws_access_key_id ENV['AWS_ACCESS_KEY_ID']
     aws_secret_access_key ENV['AWS_SECRET_ACCESS_KEY']
     aws_region ENV['AWS_REGION']
     aws_bucket_region ENV['AWS_REGION']
     # aws_sts_role_arn ENV['AWS_STS_ROLE_ARN']
+    #  Eucalyptus related additions
+    # api_endpoint ENV['EUCA_ENDPOINT']
+    # euca_compat 'path'
+    # ssl_enabled false
+    #  Azure credentials information
+    azure_tenant_id ENV['AZURE_TENANT_ID']
+    azure_client_id ENV['AZURE_CLIENT_ID']
+    azure_subscription_id ENV['AZURE_SUBSCRIPTION_ID']
+    azure_client_secret ENV['AZURE_CLIENT_ID']
+    azure_region ENV['AZURE_REGION']
+    azure_blob_account_name ENV['AZURE_BLOB_ACCOUNT_NAME']
+    azure_blob_secret_key ENV['AZURE_BLOB_SECRET_KEY']
+    #  Defaults to "miasma-orchestration-templates"
+    azure_root_orchestration_container ENV['AZURE_ROOT_ORCHESTRATION_CONTAINER']
+    #  OpenStack credentials information
+    open_stack_identity_url ENV['OPENSTACK_IDENTITY_URL']
+    open_stack_username ENV['OPENSTACK_USERNAME']
+    open_stack_user_id ENV['OPENSTACK_USER_ID']
+    open_stack_password ENV['OPENSTACK_PASSWORD']
+    open_stack_token ENV['OPENSTACK_TOKEN']
+    open_stack_region ENV['OPENSTACK_REGION']
+    open_stack_tenant_name ENV['OPENSTACK_TENANT_NAME']
+    open_stack_domain ENV['OPENSTACK_DOMAIN']
+    open_stack_project ENV['OPENSTACK_PROJECT']
+    #  Rackspace credentials information
+    rackspace_api_key ENV['RACKSPACE_API_KEY']
+    rackspace_username ENV['RACKSPACE_USERNAME']
+    rackspace_region ENV['RACKSPACE_REGION']
   end
 end
 EOF
