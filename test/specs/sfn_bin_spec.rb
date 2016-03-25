94c1e16fdc2b46bf7b3543673ca80bddedc02ebb
Chris Roberts <code@chrisroberts.org>
2016-03-24 17:28:25 -0700

    Support loading configuration files with extensions (#168)

--- test/specs/sfn_bin_spec.rb
+++ test/specs/sfn_bin_spec.rb
@@ -32,5 +32,67 @@ describe 'sfn' do
     be_sh('sfn create --fubar --debug', :fail => true).must_include 'in `validate_arguments!'
   end
 
+  describe 'configuration file' do
+
+    let(:config_dir){ File.join(File.dirname(__FILE__), 'config') }
+
+    it 'should load configuration file with no extension' do
+      result = Dir.chdir(File.join(config_dir, 'no-ext')) do
+        be_sh('sfn conf')
+      end
+      result.must_match /processing.*?:.*?false/
+    end
+
+    it 'should load Ruby configuration file with extension' do
+      result = Dir.chdir(File.join(config_dir, 'ruby-ext')) do
+        be_sh('sfn conf')
+      end
+      result.must_match /processing.*?:.*?false/
+    end
+
+    it 'should load YAML configuration file with extension' do
+      result = Dir.chdir(File.join(config_dir, 'yaml-ext')) do
+        be_sh('sfn conf')
+      end
+      result.must_match /processing.*?:.*?false/
+    end
+
+    it 'should load JSON configuration file with extension' do
+      result = Dir.chdir(File.join(config_dir, 'json-ext')) do
+        be_sh('sfn conf')
+      end
+      result.must_match /processing.*?:.*?false/
+    end
+
+    it 'should display JSON specific load error when JSON load fails' do
+      result = Dir.chdir(File.join(config_dir, 'fail-auto-json')) do
+        be_sh('sfn conf', :fail => true)
+      end
+      result.must_include 'unexpected token'
+    end
+
+    it 'should display Ruby specific load error when Ruby load fails' do
+      result = Dir.chdir(File.join(config_dir, 'fail-auto-ruby')) do
+        be_sh('sfn conf', :fail => true)
+      end
+      result.must_include 'syntax error'
+    end
+
+    it 'should display YAML specific load error when YAML load fails' do
+      result = Dir.chdir(File.join(config_dir, 'fail-auto-yaml')) do
+        be_sh('sfn conf', :fail => true)
+      end
+      result.must_include 'did not find expected node'
+    end
+
+    it 'should display stacktrace in debug mode on load error' do
+      result = Dir.chdir(File.join(config_dir, 'fail-auto-yaml')) do
+        be_sh('sfn conf --debug', :fail => true)
+      end
+      result.must_include 'Stacktrace'
+      result.must_include 'SyntaxError'
+    end
+
+  end
 
 end
