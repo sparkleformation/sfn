ba5f0b13a2bb0cf5c5b767b8ab4a978ddbad0fdc
Chris Roberts <code@chrisroberts.org>
2016-04-13 13:59:51 -0700

    Remove stack unpacking helpers. Add param validator module

--- lib/sfn/command_module/stack.rb
+++ lib/sfn/command_module/stack.rb
@@ -51,61 +51,6 @@ module Sfn
           stack
         end
 
-        # Apply all stacks from an unpacked stack
-        #
-        # @param stack_name [String] name of parent stack
-        # @param stack [Miasma::Models::Orchestration::Stack]
-        # @return [Miasma::Models::Orchestration::Stack]
-        def apply_unpacked_stack!(stack_name, stack)
-          result = provider.connection.stacks.all.find_all do |remote_stack|
-            remote_stack.name.start_with?("#{stack_name}#{UNPACK_NAME_JOINER}")
-          end.sort_by(&:name).map do |remote_stack|
-            stack.apply_stack(remote_stack)
-          end
-          unless(result.empty?)
-            stack
-          else
-            ui.error "Failed to apply requested stack. Unable to locate. (#{stack_name})"
-            raise "Failed to locate stack: #{stack_name}"
-          end
-        end
-
-        # Unpack nested stack and run action on each stack, applying
-        # the previous stacks automatically
-        #
-        # @param name [String] container stack name
-        # @param file [Hash] stack template
-        # @param action [String] create or update
-        # @return [TrueClass]
-        def unpack_nesting(name, file, action)
-          config[:apply_stacks] ||= []
-          stack_count = 0
-          file['Resources'].each do |stack_resource_name, stack_resource|
-
-            nested_stack_name = "#{name}#{UNPACK_NAME_JOINER}#{Kernel.sprintf('%0.3d', stack_count)}-#{stack_resource_name}"
-            nested_stack_template = stack_resource['Properties']['Stack']
-
-            namespace.const_get(action.to_s.capitalize).new(
-              Smash.new(
-                :print_only => config[:print_only],
-                :template => nested_stack_template,
-                :parameters => config.fetch(:parameters, Smash.new).to_smash,
-                :apply_stacks => config[:apply_stacks],
-                :options => config[:options]
-              ),
-              [nested_stack_name]
-            ).execute!
-            unless(config[:print_only])
-              config[:apply_stacks].push(nested_stack_name).uniq!
-            end
-            config[:template] = nil
-            provider.connection.stacks.reload
-            stack_count += 1
-          end
-
-          true
-        end
-
         # Prompt for parameter values and store result
         #
         # @param sparkle [SparkleFormation, Hash]
@@ -189,7 +134,7 @@ module Sfn
                 else
                   answer = default
                 end
-                validation = Sfn::Utils::StackParameterValidator.validate(answer, v)
+                validation = validate_parameter(answer, v)
                 if(validation == true)
                   config[:parameters][ns_k] = answer
                   valid = true
@@ -280,6 +225,7 @@ module Sfn
         klass.class_eval do
           extend Sfn::CommandModule::Stack::ClassMethods
           include Sfn::CommandModule::Stack::InstanceMethods
+          include Utils::StackParameterValidator
         end
       end
 
