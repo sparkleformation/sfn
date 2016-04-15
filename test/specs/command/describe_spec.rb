2ff61ae80ce0cbffcd4811f0febda9d6f59091e6
Chris Roberts <code@chrisroberts.org>
2016-04-15 09:56:47 -0700

    Refactor describe spec to use mocked http

--- test/specs/command/describe_spec.rb
+++ test/specs/command/describe_spec.rb
@@ -2,67 +2,72 @@ require_relative '../../helper'
 
 describe Sfn::Command::Describe do
 
-  let(:stream){ @stream ||= StringIO.new('') }
-  let(:connection){ @connection ||= mock }
-  let(:ui) do
-    @ui ||= Bogo::Ui.new(
-      :app_name => 'TestUi',
-      :output_to => stream,
-      :colors => false
-    )
-  end
+  let(:creds){ Smash.new }
+  let(:credentials){ Smash.new(:credentials => creds) }
 
-  let(:stack_collection){ Miasma::Models::Orchestration::Stacks.new(connection) }
-  let(:stack_collection_instances){ [stack_model] }
-  let(:resource_collection){ Miasma::Models::Orchestration::Stack::Resources.new(connection) }
-  let(:resource_collection_instances){ [resource_model] }
+  describe 'AWS' do
 
-  let(:resource_model) do
-    Miasma::Models::Orchestration::Stack::Resource.new(connection).load_data(
-      :name => 'test-resource',
-      :logical_id => 'test-resource',
-      :type => 'Test::Resource',
-      :state => :create_complete,
-      :status => 'Create Complete'
-    ).valid_state
-  end
-  let(:stack_model) do
-    Miasma::Models::Orchestration::Stack.new(connection).load_data(
-      :id => 'test-stack',
-      :name => 'test-stack',
-      :template => {},
-      :outputs => [Smash.new(:key => 'testing', :value => '123')]
-    ).valid_state
-  end
-
-  describe 'Default behavior' do
+    let(:creds){ aws_creds }
+    let(:aws_describe_stacks) do
+      res = Smash.new
+      res.set('DescribeStacksResponse', 'DescribeStacksResult', 'Stacks', 'member',
+        Smash.new(
+          'Outputs' => {
+            'member' => [
+              Smash.new('OutputKey' => 'test-output', 'OutputValue' => 'test-output-value')
+            ]
+          },
+          'CreationTime' => Time.now.xmlschema,
+          'StackName' => 'test-stack',
+          'StackId' => 'arn:aws:cloudformation:REGION:ACCT_ID:stack/test-stack/UUID',
+          'StackStatus' => 'CREATE_COMPLETE',
+          'Tags' => {
+            'member' => {
+              "Key" => 'test-tag',
+              "Value" => 'test-tag-value'
+            }
+          },
+        )
+      )
+      res
+    end
+    let(:aws_list_resources) do
+      res = Smash.new
+      res.set('ListStackResourcesResponse', 'ListStackResourcesResult', 'StackResourceSummaries', 'member', [
+        'LastUpdatedTimestamp' => Time.now.xmlschema,
+        'PhysicalResourceId' => 'TestStackResourceId',
+        'LogicalResourceId' => 'TestStackResource',
+        'ResourceStatus' => 'CREATE_COMPLETE',
+        'ResourceType' => 'Custom::TestResource'
+      ])
+      res
+    end
 
     before do
-      connection.expects(:api).returns(connection).at_least_once
-      connection.expects(:stack_all).returns(stack_collection_instances).at_least_once
-      connection.expects(:class).returns(Miasma::Models::Orchestration::Aws).at_least_once
-      connection.expects(:data).returns(Smash.new).at_least_once
-      connection.expects(:stacks).returns(stack_collection).at_least_once
-      connection.expects(:resource_all).returns(resource_collection_instances).at_least_once
-      connection.expects(:stack_reload).returns(stack_model).at_least_once
+      $mock.expects(:post).with{|url, opts|
+        opts.to_smash.get(:form, 'Action') == 'DescribeStacks'
+      }.returns(http_response(:body => aws_describe_stacks.to_json)).once
+      $mock.expects(:post).with{|url, opts|
+        opts.to_smash.get(:form, 'Action') == 'ListStackResources'
+      }.returns(http_response(:body => aws_list_resources.to_json)).once
     end
 
     it 'should display outputs' do
-      instance = command_instance(Sfn::Command::Describe, {}, ['test-stack'])
+      instance = Sfn::Command::Describe.new({:ui => ui}.merge(credentials), ['test-stack'])
       instance.execute!
       stream.rewind
       output = stream.read
-      output.must_include 'Testing'
-      output.must_include '123'
+      output.must_include 'Test Output'
+      output.must_include 'test-output-value'
     end
 
     it 'should display resources' do
-      instance = command_instance(Sfn::Command::Describe, {}, ['test-stack'])
+      instance = Sfn::Command::Describe.new({:ui => ui}.merge(credentials), ['test-stack'])
       instance.execute!
       stream.rewind
       output = stream.read
-      output.must_include 'test-resource'
-      output.must_include 'Test::Resource'
+      output.must_include 'TestStackResource'
+      output.must_include 'Custom::TestResource'
     end
 
   end
