2c8c5aec7ce104301a08da75702f12eb29a29ccb
Chris Roberts <code@chrisroberts.org>
2016-04-19 13:44:10 -0700

    Override nested stacks to provide compat stack model instances

--- lib/sfn/monkey_patch/stack/google.rb
+++ lib/sfn/monkey_patch/stack/google.rb
@@ -3,7 +3,70 @@ require 'sfn'
 module Sfn
   module MonkeyPatch
     module Stack
+      # Google specific monkey patch implementations
       module Google
+
+        # Helper module to allow nested stack behavior to function as expected
+        # internally within sfn
+        module PretendStack
+
+          # disable reload
+          def reload
+            self
+          end
+
+          # disable template load
+          def perform_template_load
+            Smash.new
+          end
+
+          # only show resources associated to this stack
+          def resources
+            collection = Miasma::Models::Orchestration::Stack::Resources.new(self)
+            collection.define_singleton_method(:perform_population) do
+              valid = stack.sparkleish_template.fetch(:resources, {}).keys
+              stack.custom[:resources].find_all{|r| valid.include?(r[:name])}.map do |attrs|
+                Miasma::Models::Orchestration::Stack::Resource.new(stack, attrs).valid_state
+              end
+            end
+            collection
+          end
+
+        end
+
+        # Return all stacks contained within this stack
+        #
+        # @param recurse [TrueClass, FalseClass] recurse to fetch _all_ stacks
+        # @return [Array<Miasma::Models::Orchestration::Stack>]
+        def nested_stacks_google(recurse=true)
+          my_template = sparkleish_template
+          n_stacks = sparkleish_template[:resources].map do |s_name, content|
+            if(content[:type] == 'sparkleformation.stack')
+              n_stack = self.class.new(api)
+              n_stack.extend PretendStack
+              n_layout = custom.fetch(:layout, {}).fetch(:resources, []).detect{|r| r[:name] == name}
+              n_layout = (n_layout || custom.fetch(:layout, {})).fetch(:resources, []).detect{|r| r[:name] == s_name} || Smash.new
+              n_stack.load_data(
+                :name => s_name,
+                :id => s_name,
+                :template => content.get(:properties, :stack),
+                :outputs => n_layout.fetch('outputs', []).map{|o_val| Smash.new(:key => o_val[:name], :value => o_val['finalValue'])},
+                :custom => {
+                  :resources => resources.all.map(&:attributes),
+                  :layout => n_layout
+                }
+              ).valid_state
+              n_stack.data[:logical_id] = s_name
+              n_stack.data[:parent_stack] = self
+              n_stack
+            end
+          end.compact
+          if(recurse)
+            (n_stacks + n_stacks.map(&:nested_stacks)).flatten.compact
+          else
+            n_stacks
+          end
+        end
 
         # @return [Hash] restructured google template
         def sparkleish_template_google
@@ -34,7 +97,16 @@ module Sfn
             end
             result
           end
-          deref.call(Smash.new(:resources => copy_template.get(:config, :content, :resources)))
+          s_template = deref.call(Smash.new(:resources => copy_template.get(:config, :content, :resources)))
+          if(s_template.empty?)
+            template.to_smash
+          else
+            layout = custom[:layout].to_smash
+            layout.delete(:resources).each do |l_resource|
+              layout.set(:resources, l_resource.delete(:name), l_resource)
+            end
+            s_template.fetch(:resources, name, :properties, :stack, s_template)
+          end
         end
 
       end
