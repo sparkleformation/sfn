3fe42f8d43ed95d596e08f55573f233f8a56c3d8
Chris Roberts <code@chrisroberts.org>
2016-05-11 15:59:36 -0700

    Update new parameter extraction location

--- lib/sfn/planner/aws.rb
+++ lib/sfn/planner/aws.rb
@@ -316,7 +316,7 @@ module Sfn
           end
           new_stack_exists = is_stack?(new_template_hash.get('Resources', stack_name, 'Type'))
           new_stack_template = new_template_hash.fetch('Resources', stack_name, 'Properties', 'Stack', Smash.new)
-          new_stack_parameters = new_stack_template.fetch('Parameters', Smash.new)
+          new_stack_parameters = new_template_hash.fetch('Resources', stack_name, 'Properties', 'Parameters', Smash.new)
           new_stack_type = new_template_hash.fetch('Resources', stack_name, 'Type',
             origin_template.get('Resources', stack_name, 'Type')
           )
