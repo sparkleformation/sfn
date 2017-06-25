09bdd9a37af5f97d480462bae4f2a9fef8286d06
Chris Roberts <code@chrisroberts.org>
2017-06-25 10:58:36 -0700

    Support processing complex type compile time parameters

--- lib/sfn/command_module/template.rb
+++ lib/sfn/command_module/template.rb
@@ -48,57 +48,67 @@ module Sfn
         def request_compile_parameter(p_name, p_config, cur_val, nested=false)
           result = nil
           attempts = 0
-          unless(cur_val || p_config[:default].nil?)
-            cur_val = p_config[:default]
-          end
-          if(cur_val.is_a?(Array))
-            cur_val = cur_val.map(&:to_s).join(',')
-          end
-          until(result && (!result.respond_to?(:empty?) || !result.empty?))
-            attempts += 1
-            if(config[:interactive_parameters] && (!nested || !p_config.key?(:prompt_when_nested) || p_config[:prompt_when_nested] == true))
-              result = ui.ask_question(
-                p_name.to_s.split('_').map(&:capitalize).join,
-                :default => cur_val.to_s.empty? ? nil : cur_val.to_s
-              )
+          parameter_type = p_config.fetch(:type, 'string').to_s.downcase.to_sym
+          if(parameter_type == :complex)
+            ui.debug "Compile time parameter `#{p_name}` is a complex type. Not requesting value from user."
+            if(cur_val.nil?)
+              raise ArgumentError.new "No value provided for `#{p_name}` parameter (Complex data type)"
             else
-              result = cur_val.to_s
+              cur_val
             end
-            case p_config.fetch(:type, 'string').to_s.downcase.to_sym
-            when :string
-              if(p_config[:multiple])
-                result = result.split(',').map(&:strip)
-              end
-            when :number
-              if(p_config[:multiple])
-                result = result.split(',').map(&:strip)
-                new_result = result.map do |item|
-                  new_item = item.to_i
-                  new_item if new_item.to_s == item
-                end
-                result = new_result.size == result.size ? new_result : []
+          else
+            unless(cur_val || p_config[:default].nil?)
+              cur_val = p_config[:default]
+            end
+            if(cur_val.is_a?(Array))
+              cur_val = cur_val.map(&:to_s).join(',')
+            end
+            until(result && (!result.respond_to?(:empty?) || !result.empty?))
+              attempts += 1
+              if(config[:interactive_parameters] && (!nested || !p_config.key?(:prompt_when_nested) || p_config[:prompt_when_nested] == true))
+                result = ui.ask_question(
+                  p_name.to_s.split('_').map(&:capitalize).join,
+                  :default => cur_val.to_s.empty? ? nil : cur_val.to_s
+                )
               else
-                new_result = result.to_i
-                result = new_result.to_s == result ? new_result : nil
+                result = cur_val.to_s
               end
-            else
-              raise ArgumentError.new "Unknown compile time parameter type provided: `#{p_config[:type].inspect}` (Parameter: #{p_name})"
-            end
-            valid = validate_parameter(result, p_config.to_smash)
-            unless(valid == true)
-              result = nil
-              valid.each do |invalid_msg|
-                ui.error invalid_msg.last
+              case parameter_type
+              when :string
+                if(p_config[:multiple])
+                  result = result.split(',').map(&:strip)
+                end
+              when :number
+                if(p_config[:multiple])
+                  result = result.split(',').map(&:strip)
+                  new_result = result.map do |item|
+                    new_item = item.to_i
+                    new_item if new_item.to_s == item
+                  end
+                  result = new_result.size == result.size ? new_result : []
+                else
+                  new_result = result.to_i
+                  result = new_result.to_s == result ? new_result : nil
+                end
+              else
+                raise ArgumentError.new "Unknown compile time parameter type provided: `#{p_config[:type].inspect}` (Parameter: #{p_name})"
+              end
+              valid = validate_parameter(result, p_config.to_smash)
+              unless(valid == true)
+                result = nil
+                valid.each do |invalid_msg|
+                  ui.error invalid_msg.last
+                end
+              end
+              if(result.nil? || (result.respond_to?(:empty?) && result.empty?))
+                if(attempts > MAX_PARAMETER_ATTEMPTS)
+                  ui.fatal "Failed to receive allowed parameter! (Parameter: #{p_name})"
+                  exit 1
+                end
               end
             end
-            if(result.nil? || (result.respond_to?(:empty?) && result.empty?))
-              if(attempts > MAX_PARAMETER_ATTEMPTS)
-                ui.fatal "Failed to receive allowed parameter! (Parameter: #{p_name})"
-                exit 1
-              end
-            end
+            result
           end
-          result
         end
 
         # @return [Array<SparkleFormation::SparklePack>]
