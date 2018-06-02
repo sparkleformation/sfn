972fd39f96d45f663ea818752a0f81965c3b6c54
Chris Roberts <code@chrisroberts.org>
2018-06-02 11:21:20 -0700

    Do not remove diffs options on plan command

--- lib/sfn/config/plan.rb
+++ lib/sfn/config/plan.rb
@@ -9,9 +9,6 @@ module Sfn
       # Also, since this is a plan command, we are always running
       # a plan, because that's the command.
       attributes.delete(:plan)
-      # Disable raw diff output since this will be coming from the
-      # miasma generic API
-      attributes.delete(:diffs)
 
       attribute(
         :plan_name, String,
