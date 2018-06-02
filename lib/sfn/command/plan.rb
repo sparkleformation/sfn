0ea4626eb8fc5520d7bc68447300d5356718b06b
Chris Roberts <code@chrisroberts.org>
2018-06-02 11:20:49 -0700

    Handle custom named plans correctly. Disable processing when using existing plan.

--- lib/sfn/command/plan.rb
+++ lib/sfn/command/plan.rb
@@ -23,39 +23,13 @@ module Sfn
 
         return display_plan_lists(stack) if config[:list]
 
-        config[:compile_parameters] ||= Smash.new
-
-        if config[:file]
-          s_name = [name]
-
-          c_setter = lambda do |c_stack|
-            if c_stack.outputs
-              compile_params = c_stack.outputs.detect do |output|
-                output.key == "CompileState"
-              end
-            end
-            if compile_params
-              compile_params = MultiJson.load(compile_params.value)
-              c_current = config[:compile_parameters].fetch(s_name.join("__"), Smash.new)
-              config[:compile_parameters][s_name.join("__")] = compile_params.merge(c_current)
-            end
-            c_stack.nested_stacks(false).each do |n_stack|
-              s_name.push(n_stack.data.fetch(:logical_id, n_stack.name))
-              c_setter.call(n_stack)
-              s_name.pop
-            end
-          end
-
-          if stack && stack.persisted?
-            c_setter.call(stack)
-          end
-
-          ui.debug "Compile parameters - #{config[:compile_parameters]}"
-          file = load_template_file(:stack => stack)
-          stack_info << " #{ui.color("Path:", :bold)} #{config[:file]}"
-        else
-          file = stack.template.dup
+        if config[:plan_name]
+          # ensure custom attribute is dirty so we can modify
+          stack.custom = stack.custom.dup
+          stack.custom[:plan_name] = config[:plan_name]
         end
+
+        use_existing = false
 
         unless config[:print_only]
           ui.info "#{ui.color("SparkleFormation:", :bold)} #{ui.color("plan", :green)}"
@@ -66,56 +40,91 @@ module Sfn
               ui.info "Destroying existing plan to generate new plan"
               stack.plan.destroy
             rescue Bogo::Ui::ConfirmationDeclined
-              ui.info "Using existing stack plan"
+              ui.info "Loading existing stack plan..."
+              use_existing = true
             end
           end
         end
 
-        unless file
-          if config[:template]
-            file = config[:template]
-            stack_info << " #{ui.color("(template provided)", :green)}"
+        unless use_existing
+          config[:compile_parameters] ||= Smash.new
+
+          if config[:file]
+            s_name = [name]
+
+            c_setter = lambda do |c_stack|
+              if c_stack.outputs
+                compile_params = c_stack.outputs.detect do |output|
+                  output.key == "CompileState"
+                end
+              end
+              if compile_params
+                compile_params = MultiJson.load(compile_params.value)
+                c_current = config[:compile_parameters].fetch(s_name.join("__"), Smash.new)
+                config[:compile_parameters][s_name.join("__")] = compile_params.merge(c_current)
+              end
+              c_stack.nested_stacks(false).each do |n_stack|
+                s_name.push(n_stack.data.fetch(:logical_id, n_stack.name))
+                c_setter.call(n_stack)
+                s_name.pop
+              end
+            end
+
+            if stack && stack.persisted?
+              c_setter.call(stack)
+            end
+
+            ui.debug "Compile parameters - #{config[:compile_parameters]}"
+            file = load_template_file(:stack => stack)
+            stack_info << " #{ui.color("Path:", :bold)} #{config[:file]}"
           else
-            stack_info << " #{ui.color("(no template update)", :yellow)}"
-          end
-        end
-        unless config[:print_only]
-          ui.info "  -> #{stack_info}"
-        end
-        if file
-          if config[:print_only]
-            ui.puts format_json(parameter_scrub!(template_content(file)))
-            return
+            file = stack.template.dup
           end
 
-          original_parameters = stack.parameters
+          unless file
+            if config[:template]
+              file = config[:template]
+              stack_info << " #{ui.color("(template provided)", :green)}"
+            else
+              stack_info << " #{ui.color("(no template update)", :yellow)}"
+            end
+          end
+          unless config[:print_only]
+            ui.info "  -> #{stack_info}"
+          end
+          if file
+            if config[:print_only]
+              ui.puts format_json(parameter_scrub!(template_content(file)))
+              return
+            end
 
-          apply_stacks!(stack)
+            original_parameters = stack.parameters
 
-          populate_parameters!(file, :current_parameters => stack.root_parameters)
+            apply_stacks!(stack)
 
-          stack.parameters = config_root_parameters
+            populate_parameters!(file, :current_parameters => stack.root_parameters)
 
-          if config[:upload_root_template]
-            upload_result = store_template(name, file, Smash.new)
-            stack.template_url = upload_result[:url]
+            stack.parameters = config_root_parameters
+
+            if config[:upload_root_template]
+              upload_result = store_template(name, file, Smash.new)
+              stack.template_url = upload_result[:url]
+            else
+              stack.template = parameter_scrub!(template_content(file, :scrub))
+            end
           else
-            stack.template = parameter_scrub!(template_content(file, :scrub))
+            apply_stacks!(stack)
+            original_parameters = stack.parameters
+            populate_parameters!(stack.template, :current_parameters => stack.root_parameters)
+            stack.parameters = config_root_parameters
           end
+
+          # Set options defined within config into stack instance for update request
+
+          ui.info "  -> Generating plan information..."
         else
-          apply_stacks!(stack)
-          original_parameters = stack.parameters
-          populate_parameters!(stack.template, :current_parameters => stack.root_parameters)
-          stack.parameters = config_root_parameters
+          ui.info "  -> Loading plan information..."
         end
-
-        # Set options defined within config into stack instance for update request
-
-        if config[:plan_name]
-          stack.custom[:plan_name] = config[:plan_name]
-        end
-
-        ui.info "  -> Generating plan information..."
 
         plan = stack.plan || stack.plan_generate
 
@@ -184,29 +193,29 @@ module Sfn
         end
         ui.info "Plans for stack: #{ui.color(stack.name, :bold)}\n"
         n_width = i_width = s_width = 0
-        plans.each do |plan|
+        plan_info = plans.map do |plan|
+          plan_id = plan.id.to_s.split("/").last
           n_width = plan.name.to_s.length if plan.name.to_s.length > n_width
-          i_width = plan.id.to_s.length if plan.id.to_s.length > i_width
+          i_width = plan_id.to_s.length if plan_id.length > i_width
           s_width = plan.state.to_s.length if plan.state.to_s.length > s_width
+          [plan.name, plan_id, plan.state]
         end
-        plans.each do |plan|
-          table = ui.table(self) do
-            table(:border => false) do
-              row(:header => true) do
-                column "Plan Name", :width => n_width + 5
-                column "Plan ID", :width => i_width + 5
-                column "Plan State", :width => s_width + 5
-              end
-              plans.sort_by(&:name).each do |plan|
-                row do
-                  column plan.name
-                  column plan.id
-                  column plan.state
+        table = ui.table(self) do
+          table(:border => false) do
+            row(:header => true) do
+              column "Plan Name", :width => n_width + 5
+              column "Plan ID", :width => i_width + 5
+              column "Plan State", :width => s_width + 5
+            end
+            plan_info.sort_by(&:first).each do |plan|
+              row do
+                plan.each do |item|
+                  column item
                 end
               end
             end
-          end.display
-        end
+          end
+        end.display
       end
     end
   end
